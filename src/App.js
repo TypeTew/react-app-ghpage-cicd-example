7d2a444e19cd213c4dc9bea01338247d498c9186
TypeTew <tewsk132@gmail.com>
2025-08-24 14:33:57 +0700

    Fix typo in App.js description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js _ Github Page</code> and save to reload.
+          Edit <code>src/App.js _ Github Page TypeTew</code> and save to reload.
         </p>
         <a
           className="App-link"
